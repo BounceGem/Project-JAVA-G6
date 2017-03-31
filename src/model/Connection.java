1cee806e6b11d90ac87663aa0f05d30d19004247
ketaro16 <aphiphit.ketaro@mail.kmutt.ac.th>
2017-03-31 18:05:55 +0700

    Edit ConnectionDB

--- src/model/Connection.java
+++ src/model/Connection.java
@@ -1,9 +1,20 @@
-
 package model;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connection {
-    public static void conn4Me(){
-        Class.forName("")
+ public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://10.4.56.21:3306/facility","facility","facility");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println(ex);
+        }
+        return conn;
+    }
+    public static void main(String[] args) {
+        Connection con = getConnection();
     }
 }
